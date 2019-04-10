2fc966c1d059243505171c830f20e593ebcc9d7a
Oliver Abreu <oliverabreu@Olivers-MacBook.attlocal.net>
2019-04-09 22:08:57 -0400

    This commit should create the logic for implementing user registration and user login

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require("bcryptjs"); 
+const jwt = require("jsonwebtoken"); 
+const db = require('../database/dbConfig'); 
+
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,10 +14,33 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 10);
+  db.insertUser(user)
+    .then(user => {
+      res.status(201).json({ message: "Registration successful" });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Registration failed" });
+    });
 }
 
 function login(req, res) {
   // implement user login
+  const user = req.body;
+  db.userLogin(user.username)
+    .then(user => {
+      if (user && bcrypt.compareSync(user.password, user.password)) {
+        const token = generateToken(user);
+        console.log("users token", token);
+        res.status(200).json({ message: "You are logged in!", token });
+      } else {
+        res.status(404).json({ message: "Incorrect username or password" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function getJokes(req, res) {
