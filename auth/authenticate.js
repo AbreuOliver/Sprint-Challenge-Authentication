6e9aacbb49df1fcce78d6d5711189c11435c07e0
Oliver Abreu <oliverabreu@Olivers-MacBook.attlocal.net>
2019-04-13 12:50:17 -0400

    This commit should create and export the generateToken function

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -6,19 +6,16 @@ const jwtKey =
 
 // quickly see what this file exports
 module.exports = {
-  authenticate,
+  authenticate, generateToken
 };
 
 // implementation details
 function authenticate(req, res, next) {
   const token = req.get('Authorization');
-
   if (token) {
     jwt.verify(token, jwtKey, (err, decoded) => {
       if (err) return res.status(401).json(err);
-
       req.decoded = decoded;
-
       next();
     });
   } else {
@@ -26,4 +23,16 @@ function authenticate(req, res, next) {
       error: 'No token provided, must be set on the Authorization Header',
     });
   }
+}
+
+// CREATE JSON WEB TOKEN
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  }
+
+  return jwt.sign(payload, jwtKey, options)
 }
