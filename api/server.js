d8188cddeb522062cdefa6f13083fdaa91dd8e41
Oliver Abreu <oliverabreu@Olivers-MacBook.attlocal.net>
2019-04-13 12:36:43 -0400

    This commit should set the port and test the initial route for the server

--- api/server.js
+++ api/server.js
@@ -10,6 +10,11 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+// TEST ROUTE
+server.get('/', (req, res) => {
+    res.send("Yup, the server is working (second attempt)");
+});
+
 configureRoutes(server);
 
 module.exports = server;
